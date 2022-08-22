1fcbe30ce026d911e2222bb069f907d66626c20f
Евгений Серов <serov@hotmail.com>
2022-08-22 12:53:58 +0300

    Task descriptions added

--- Workshop 1/Task 2/Program.cs
+++ Workshop 1/Task 2/Program.cs
@@ -1,4 +1,5 @@
-﻿Console.Write("Введите первое число: ");
+﻿// Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+Console.Write("Введите первое число: ");
 int num1 = int.Parse(Console.ReadLine());
 Console.Write("Введите второе число: ");
 int num2 = int.Parse(Console.ReadLine());
