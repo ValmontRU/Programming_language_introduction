d10c5f4d6fb821781c7443bf7f4fd4bd7989b373
Евгений Серов <serov@hotmail.com>
2022-08-28 19:45:07 +0300

    Tasks from Workshop 3 added

---
+++ Workshop 3/Task 23/Program.cs
@@ -0,0 +1,19 @@
+﻿// Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+
+Console.Write("Введите любое число, большее или равное 1: ");
+int num = int.Parse(Console.ReadLine());
+if (num >= 1)
+{
+    int x = 1;
+    double cube = 1;
+    while (x < num + 1)
+    {
+        cube = Math.Pow(x, 3);
+        Console.Write(cube + " ");
+        x++;
+    }
+}
+else
+{
+    Console.Write("Вы ввели неправильное число");
+}
