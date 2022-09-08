a4d55b9213d92e70c148ec7cc234efc21b149290
Евгений Серов <serov@hotmail.com>
2022-09-08 16:26:09 +0300

    Tasks from Workshop 6 added

---
+++ Workshop 6/Task 41/Program.cs
@@ -0,0 +1,24 @@
+﻿// Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+
+int PositiveCount(int numbers)
+{
+    int count = 0;
+    for (int i = 0; i < numbers; i++)
+    {
+        Console.WriteLine("Введите любое число:");
+        int x = int.Parse(Console.ReadLine());
+        if (x > 0) count++;
+    }
+    return count;
+}
+
+Console.Write("Введите положительное число M: ");
+int M = int.Parse(Console.ReadLine());
+if (M > 0)
+{
+    Console.Write($"Количество положительных чисел равно {PositiveCount(M)}");
+}
+else
+{
+    Console.Write("Вы ввели неправильное число M");
+}
---
+++ Workshop 6/Task 43/Program.cs
@@ -0,0 +1,34 @@
+﻿// Напишите программу, которая найдёт точку пересечения двух прямых, заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются пользователем.
+
+double Intersection(double a, double b, double c, double d)
+{
+    if (b == d)
+    {
+        if (a == c)
+        {
+            Console.Write("Прямые совпадают");
+        }
+        else
+        {
+            Console.Write("Прямые параллельны");
+        }
+    }
+    else
+    {
+        double x = (c - a) / (b - d);
+        double y = b * x + a;
+        Console.Write($"({x}, {y})");
+    }
+    return 0;
+}
+
+Console.Write("Введите b1: ");
+double b1 = double.Parse(Console.ReadLine());
+Console.Write("Введите k1: ");
+double k1 = double.Parse(Console.ReadLine());
+Console.Write("Введите b2: ");
+double b2 = double.Parse(Console.ReadLine());
+Console.Write("Введите k2: ");
+double k2 = double.Parse(Console.ReadLine());
+
+Intersection(b1, k1, b2, k2);
