48134047b4457fa717498d709e655528e48f12da
Евгений Серов <serov@hotmail.com>
2022-09-18 17:46:19 +0300

    Tasks from Workshop 9 added

---
+++ Workshop 9/Task 66/Program.cs
@@ -0,0 +1,15 @@
+﻿// Задайте значения M и N.
+// Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+
+int ElementsSum(int m, int n)
+{
+    return m <= n ? m + ElementsSum(m + 1, n) : 0;
+}
+
+Console.Write("Введите натуральное число M: ");
+int numM = int.Parse(Console.ReadLine());
+Console.Write("Введите натуральное число N: ");
+int numN = int.Parse(Console.ReadLine());
+
+if (numM < 1 || numN < 1) Console.Write("Вы ввели неправильное число");
+else Console.Write(ElementsSum(numM, numN));
---
+++ Workshop 9/Task 68/Program.cs
@@ -0,0 +1,17 @@
+﻿// Даны два неотрицательных числа m и n.
+// Напишите программу вычисления функции Аккермана с помощью рекурсии.
+
+int Ackermann(int m, int n)
+{
+    if (m == 0) return n + 1;
+    else if ((m != 0) && (n == 0)) return Ackermann(m - 1, 1);
+    else return Ackermann(m - 1, Ackermann(m, n - 1));
+}
+
+Console.Write("Введите неотрицательное число M: ");
+int numM = int.Parse(Console.ReadLine());
+Console.Write("Введите неотрицательное число N: ");
+int numN = int.Parse(Console.ReadLine());
+
+if (numM < 0 || numN < 0) Console.Write("Вы ввели неправильное число");
+else Console.Write(Ackermann(numM, numN));
